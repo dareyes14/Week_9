7834effe71b186221b6ea537e887a6dd301131dc
dareyes14 <dareyes14@cps.edu>
2024-10-24 12:49:33 -0500

    Add files via upload

---
+++ bell_ringer_boolean_challenge.py
@@ -0,0 +1,41 @@
+# #Exercise 1:
+
+# language = input("What is your favorite programming language?")
+# if language == 'python':
+#     print("You love Python!")
+# else: 
+#     print("Diferent choice")
+
+# #Exersise 2:
+
+# grade = int(input('What is your score?'))
+# if grade >= 90:
+#     print('A')
+# elif grade >= 80 and grade <90:
+#     print('B')
+# elif grade >= 70 and grade <80:
+#     print('C')
+# elif grade >= 60 and grade <70:
+#     print('D')
+# else:
+#     print('F')
+
+# Exercise 3
+
+# username = input ("Please Enter Username:")
+
+# if username == 'admin':
+#     print('Welcome Admin!')
+# else:
+#     print('Please Enter Username:')
+
+#Exercise 4
+
+a = [1,2,3]
+b = [1,2,3]
+if a == b:
+    print('true')
+else:
+    print('false')
+
+#Exercise 5
